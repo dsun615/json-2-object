140685a09d1a9ab7ffd855ae6762d6da4e808205
PodolyanVladimir <vladimir.podolyan64@gmail.com>
2023-07-26 00:52:56 +0300

    name changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="json_to_model",
+    name="json-to-object",
     version="0.0.1",
     author="Original author: Denna Sun, Maintained by Vladimir Podolian",
     author_email="vladimir.podlyan64@gmail.com",
